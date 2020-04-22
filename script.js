980f6ec5231cee89df781f8fb5bc4597aa21adb9
Sean Yeo <seansqyeo@gmail.com>
2020-04-21 17:01:46 -0700

    implement computerPlay function

---
+++ script.js
@@ -0,0 +1,14 @@
+function computerPlay() {
+    let choice = Math.round(Math.random() * 3);
+    if (choice == 1) {
+        return "Rock";
+    } else if (choice == 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function rockPaperScissors(playerSelection, computerSelection) {
+
+}
